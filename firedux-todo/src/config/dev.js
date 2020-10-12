80005831bdd2026837a30896da6dce38b040dd4b
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-12 16:42:33 +0500

    Firebase Todo App

---
+++ firedux-todo/src/config/dev.js
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyBopBHcRLORFpqHMTvW2UQfFKFOLz6DiKg",
+  authDomain: "pwademo-app.firebaseapp.com",
+  databaseURL: "https://pwademo-app.firebaseio.com",
+  projectId: "pwademo-app",
+  storageBucket: "pwademo-app.appspot.com",
+  messagingSenderId: "654039622001",
+  appId: "1:654039622001:web:3b10fcc1750d365ea516d0",
+  measurementId: "G-6DCL45MZ26"
+}
