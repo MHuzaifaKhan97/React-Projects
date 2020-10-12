117f279be1d6408c2b93f822888ce6b682a62f6b
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2020-10-12 16:40:15 +0500

    Simple PWA using React

---
+++ pwa-weather-app/build/precache-manifest.8df596222c5fb7aa307a27f3abc67112.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b9df6c7f09c926fc8682ec2324a8de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a66037c61e2fcea0068",
+    "url": "/static/css/main.c1e46d96.chunk.css"
+  },
+  {
+    "revision": "243b8fdfab42f290c7a1",
+    "url": "/static/js/2.d49fe522.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d49fe522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a66037c61e2fcea0068",
+    "url": "/static/js/main.edb2b806.chunk.js"
+  },
+  {
+    "revision": "9477723e8b43b199ec93",
+    "url": "/static/js/runtime-main.ba3ed84e.js"
+  }
+]);
